884c23767d72c9b27940d7ecb6a0eac7a86a436f
Connor Dickson <68393151+cdcksn@users.noreply.github.com>
2025-07-16 23:35:20 +0000

    02_05_E

---
+++ window_function_practice.sql
@@ -0,0 +1,46 @@
+with sales_order as (
+select EmpID
+  , OrderNum
+  , OrderDate
+  , row_number() over(partition by EmpID order by OrderDate) as row_nbr
+from sales_data_cleaned
+)
+
+select * 
+from sales_order 
+where row_nbr = 1;
+
+select EmpID
+  , OrderNum
+  , OrderDate
+  , row_number() over(partition by EmpID order by OrderDate) as row_nbr
+from sales_data_cleaned
+qualify row_nbr = 1;
+
+select EmpID  
+  , sum(OrderTotal) as OrderTotalSum
+  , rank() over(order by sum(OrderTotal) desc) as OrderTotalRank
+from sales_data_cleaned
+group by EmpID
+order by OrderTotalRank; 
+
+select EmpID  
+  , count(OrderTotal) as OrderTotalCount
+  , rank() over(order by count(OrderTotal) desc) as OrderTotalRank
+from sales_data_cleaned
+group by EmpID
+order by OrderTotalRank; 
+
+select EmpID  
+  , count(OrderTotal) as OrderTotalCount
+  , dense_rank() over(order by count(OrderTotal) desc) as OrderTotalRank
+from sales_data_cleaned
+group by EmpID
+order by OrderTotalRank; 
+
+select date(OrderDate, 'start of month') as OrderMonth
+  , sum(OrderTotal) as OrderTotalSum
+  , lag(sum(OrderTotal), 1, 0) over(order by date(OrderDate, 'start of month')) as PreviousMonthOrderTotalSum
+  , (sum(OrderTotal) - lag(sum(OrderTotal), 1, 0) over(order by date(OrderDate, 'start of month'))) / sum(OrderTotal) as PercentChangeOrderTotalSum
+from sales_data_cleaned
+group by OrderMonth; 
